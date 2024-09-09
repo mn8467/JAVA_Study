9fd16db0817852d0fb333d56b49bd49550b5618e
mn8467 <mn8467@naver.com>
2024-09-09 23:58:00 +0900

    bigyo

--- java-start/src/Operator/Comp1.java
+++ java-start/src/Operator/Comp1.java
@@ -5,5 +5,14 @@ public class Comp1 {
         int a = 2;
         int b = 3;
         System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+        //결과를 boolean 변수에 담을 수 있다
+        boolean result = a == b ;
+        System.out.println(result);
     }
 }
