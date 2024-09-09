8d6d440d4ed304482a6919638793d25c24654d19
mn8467 <mn8467@naver.com>
2024-09-10 01:04:29 +0900

    연산자

---
+++ java-start/src/Operator/ex/OperationEx3.java
@@ -0,0 +1,9 @@
+package Operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int  score = 70;
+        boolean result = 80 <= score && score <= 100;
+        System.out.println(result);
+    }
+}
